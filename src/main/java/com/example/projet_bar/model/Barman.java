a6dd6e140d5b71bccb1504122108596444f31e60
zidixx <nathanloubet12@gmail.com>
2025-12-12 12:30:19 +0100

    update

--- src/main/java/com/example/projet_bar/model/Barman.java
+++ src/main/java/com/example/projet_bar/model/Barman.java
@@ -1,4 +1,27 @@
-package com.example.projet_bar.model;
+package com.example.projet_bar.Model;
+
 
 public class Barman extends Employe {
+
+    private int competenceMixologie;
+
+    /**
+     * Constructeur pour un Barman.
+     * @param nom Le nom de l'employé
+     * @param anciennete L'ancienneté (années)
+     * @param competenceMixologie Niveau de compétence en mixologie (1 à 10)
+     */
+    public Barman(String nom, int anciennete, int competenceMixologie) {
+        super(nom, anciennete);
+        this.competenceMixologie = competenceMixologie;
+    }
+
+    @Override
+    public void travailler() {
+        System.out.println(this.getNom() + " prépare les boissons avec une compétence de " + this.competenceMixologie + ".");
+    }
+
+    public int getCompetenceMixologie() {
+        return competenceMixologie;
+    }
 }
--- src/main/java/com/example/projet_bar/model/Serveur.java
+++ src/main/java/com/example/projet_bar/model/Serveur.java
@@ -1,4 +1,31 @@
-package com.example.projet_bar.model;
+package com.example.projet_bar.Model;
+
 
 public class Serveur extends Employe {
+
+    private int vitesseService;
+
+    /**
+     * Constructeur pour un Serveur.
+     * @param nom Le nom de l'employé
+     * @param anciennete L'ancienneté (années)
+     * @param vitesseService Niveau de compétence en vitesse (1 à 10)
+     */
+    public Serveur(String nom, int anciennete, int vitesseService) {d
+        super(nom, anciennete);
+        this.vitesseService = vitesseService;
+    }
+
+    @Override
+    public void travailler() {
+        System.out.println(this.getNom() + " sert les commandes avec une vitesse de " + this.vitesseService + ".");
+    }
+
+    public void prendreCommande(Client client, Commande commande) {
+        System.out.println(this.getNom() + " prend la commande #" + commande.getId() + " du client " + client.getNom());
+    }
+
+    public int getVitesseService() {
+        return vitesseService;
+    }
 }
