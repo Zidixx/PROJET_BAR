84f9e3afd060b0d8fa43477e98f7b3f3924492ee
zidixx <nathanloubet12@gmail.com>
2025-12-12 12:30:19 +0100

    update

--- src/main/java/com/example/projet_bar/model/Employe.java
+++ src/main/java/com/example/projet_bar/model/Employe.java
@@ -1,4 +1,23 @@
-package com.example.projet_bar.model;
+package com.example.projet_bar.Model;
 
 public class Employe {
+
+    private int id;
+    protected String nom;
+    private int anciennete;
+    private static int compteurId = 100;
+
+    public Employe(String nom, int anciennete) {
+        this.id = compteurId++;
+        this.nom = nom;
+        this.anciennete = anciennete;
+    }
+
+    public void travailler() {
+        System.out.println(this.nom + " travaille...");
+    }
+
+    public int getId() { return id; }
+    public String getNom() { return nom; }
+    public int getAnciennete() { return anciennete; }
 }
